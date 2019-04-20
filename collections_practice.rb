ebea297859932997e2dfeefc9ea2a36ffc832fa6
Laura Hoffman <lhoffmanwg@gmail.com>
2019-04-20 04:22:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
-# your code goes here
+require 'pry'
+
+def count_elements(name_ary)
+  new_hash = {}
+  new_ary = [] 
+  
+  name_ary.each do |name_hash|
+    name_hash.each do |name_key, name_value|
+      count = new_hash[name_value]
+      if count == nil
+        new_hash[name_value] = 1 
+      else
+        new_hash[name_value] = count + 1 
+      end  
+      binding.pry
+    end 
+  end
